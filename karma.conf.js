37dd8b7e90451f77dc4591a4c7a76e2dc66ff58b
Nicolas Gleichgerrcht <nicolas@restorando.com>
2016-01-03 20:10:01 -0300

    Add testing capabilities

---
+++ karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['browserify', 'jasmine'],
+    browsers: ['PhantomJS'],
+    colors: true,
+    reporters: ['progress'],
+
+    files: [
+      // add files to load to the browser like angular or jquery
+      'src/**/*.js',
+      'test/**/*_spec.js'
+    ],
+
+    exclude: [
+    ],
+
+    preprocessors: {
+      'src/**/*.js': ['browserify'],
+      'test/**/*_spec.js': ['browserify']
+    },
+
+    browserify: {
+       debug: true,
+       transform: ['babelify']
+    },
+
+    plugins: [
+      'karma-browserify',
+      'karma-jasmine',
+      'karma-phantomjs-launcher'
+    ]
+
+  });
+};
